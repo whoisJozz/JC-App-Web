bdb1b08f6fcd91e20874e64c71b6e50f16e534c5
whoisJozz <jlakaran9@gmail.com>
2026-07-13 14:02:43 -0600

    Cambios en los .bat de encendido y apagado

--- apagar_jcapp.bat
+++ apagar_jcapp.bat
@@ -1,9 +1,16 @@
 @echo off
-echo =========================================
-echo APAGANDO SERVIDORES DE JC APP 
-echo =========================================
+color 0C
+title JC App - Pausar Servidor
+echo ==========================================
+echo        PAUSANDO SERVIDORES JC APP
+echo ==========================================
 echo.
-docker-compose down
+echo Congelando contenedores de forma segura...
 echo.
-echo ¡Servidores apagados exitosamente!
+docker-compose stop
+echo.
+echo ==========================================
+echo ¡SISTEMA PAUSADO CORRECTAMENTE!
+echo (Tus contenedores y redes siguen en la maquina)
+echo ==========================================
 pause
